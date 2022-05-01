e0877618851ee92f8ef29bcd5982eb13b65f018a
sachix1001 <sachix1001@gmail.com>
2022-05-01 20:45:50 +1000

    add onClick functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import styled from "styled-components";
+import "./App.css";
+
+const DisplayText: { [key: string]: string } = {
+  setTime:
+    "Please input the amount of time in seconds between emitting numbers and their frequency",
+  firstNumber: "Please enter the first number",
+  nextNumber: "Please enter the next number",
+  halt: "Timer halted",
+  resume: "Timer resumed",
+  quit: "Thanks for playing, press any key to exit.",
+};
+
+interface Frequency {
+  [key: string]: number;
+}
 
 function App() {
+  const [textKey, setTextKey] = React.useState<keyof typeof DisplayText>(
+    "setTime"
+  );
+  const [frequency, setFrequency] = React.useState<Frequency | undefined>(
+    undefined
+  );
+  const [time, setTime] = React.useState<number>(0);
+  const [input, setInput] = React.useState<number | undefined>();
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput((value) =>
+      e.target.validity.valid ? Number(e.target.value) : value
+    );
+  };
+
+  const handleEnter = (e: React.MouseEvent<HTMLElement>) => {
+    switch (textKey) {
+      // set printing frequency
+      case "setTime":
+        if (input) setTime(input);
+        setTextKey("firstNumber");
+        setInput(undefined);
+        break;
+
+      // update input number to frequency dictionary
+      case "firstNumber":
+        if (input) setFrequency({ input: 1 });
+        setTextKey("nextNumber");
+        setInput(undefined);
+        break;
+
+      // update input number to frequency dictionary
+      // copy frequency obj to trigger state update
+      case "nextNumber":
+        if (input && frequency) {
+          const incrementedFrequency = { ...frequency };
+          incrementedFrequency[input] = (incrementedFrequency[input] + 1) | 1;
+          setFrequency(incrementedFrequency);
+        }
+        setInput(undefined);
+        break;
+    }
+  };
+
+  const handleHalt = () => {
+    setTextKey("halt");
+  };
+  const handleResume = () => {
+    setTextKey("resume");
+  };
+  const handleQuite = () => {
+    setTextKey("quit");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h5>{DisplayText[textKey]}</h5>
+        <h5>{JSON.stringify(frequency)}</h5>
+        <input onChange={handleInput} type="text" pattern="[0-9]*"></input>
+        <button onClick={handleEnter}>Enter</button>
+        <button onClick={handleHalt}>Halt</button>
+        <button onClick={handleResume}>Resume</button>
+        <button onClick={handleQuite}>Quit</button>
       </header>
     </div>
   );
